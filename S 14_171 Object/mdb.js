906be73c764cfe5ce72e54dce0145570bdb9b696
GowthamNM <66351888+GowthamNM@users.noreply.github.com>
2020-06-11 09:14:30 +0530

    HTML,JS(Object)

---
+++ S 14_171 Object/mdb.js
@@ -0,0 +1,27 @@
+var moviedb=[
+{
+	title:"007",
+	haswatched: "True",
+	Rating: 5
+},
+{
+        title:"2012",
+	haswatched: "False",
+	Rating: 4.5
+},
+{
+	title:"24",
+	haswatched: "True",
+	Rating: 5
+},
+{
+	title:"96",
+	haswatched: "False",
+	Rating: 3.5
+},
+{
+	title:"1997",
+	haswatched: "True",
+	Rating: 3.5
+},
+]
